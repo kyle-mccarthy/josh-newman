a02bf3d7dba2e9f5e0dd4a471e345cce62deaecf
Kyle McCarthy <km@kylemccarthy.io>
2018-09-23 16:09:00 -0500

    include ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    // First application
+    {
+      name      : 'josh',
+      script    : './.next/production-server/index.js',
+      env_production: {
+        PORT: 3200,
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'kyle',
+      host : '159.89.143.41',
+      ref  : 'origin/master',
+      repo : 'git@github.com:kyle-mccarthy/josh-newman.git',
+      path : '/var/www/kyle/sites/josh-newman',
+      ['post-deploy'] : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
